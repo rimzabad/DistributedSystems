bbc7810d9495dc1efb0e7e4c3377d890c7f11867
Rim Zabad <rimzabad26@gmail.com>
2023-11-16 14:37:27 +0200

    Add DistributedWeatherApplication project to repository

---
+++ DistributedWeatherApplication/DistributedWeatherApplication/Models/WeatherData.cs
@@ -0,0 +1,24 @@
+﻿namespace DistributedWeatherApplication.Models
+{
+    public class WeatherData
+    {
+        public Location Location { get; set; }
+        public Current Current { get; set; }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+    }
+
+    public class Current
+    {
+        public double Temp_C { get; set; }
+        public Condition Condition { get; set; }
+    }
+
+    public class Condition
+    {
+        public string Text { get; set; }
+    }
+}
