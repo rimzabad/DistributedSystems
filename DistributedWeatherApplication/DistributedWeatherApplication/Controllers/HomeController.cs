bff9d9f16f1b055675ff963593bc4baf8aa5b5bd
Rim Zabad <rimzabad26@gmail.com>
2023-11-16 14:15:18 +0200

    Initial commit: Adding .NET web application

---
+++ DistributedWeatherApplication/DistributedWeatherApplication/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DistributedWeatherApplication.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public HomeController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        // Replace "YOUR_API_KEY" with your OpenWeatherMap API key
+        string apiKey = "86f3fce50c404680921112037231611";
+        string city = "Beirut"; // Replace with the desired city name
+
+        var weatherData = await GetWeatherDataAsync(apiKey, city);
+
+        return View(weatherData);
+    }
+
+    private async Task<WeatherData> GetWeatherDataAsync(string apiKey, string city)
+    {
+        using (var httpClient = _httpClientFactory.CreateClient())
+        {
+            string apiUrl = $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}";
+
+            try
+            {
+                var response = await httpClient.GetFromJsonAsync<WeatherData>(apiUrl);
+                return response;
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+        }
+    }
+
+}
---
+++ DistributedWeatherApplication/DistributedWeatherApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DistributedWeatherApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
